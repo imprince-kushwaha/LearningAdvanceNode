e6db6e32422577475cf919899386b2a2eca6e993
imprince-kushwaha <mijadon593@hidelux.com>
2024-10-14 13:05:54 +0530

    Initialize the database

---
+++ src/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const connectDB = require('./db/database')
+const app = express();
+
+const User = require("./models/user.model")
+
+app.post('/signup', async (req, res) => {
+    // const userObj={
+    //     firstName:"Prince",
+    //     lastName:"Kushwaha",
+    //     emailId:"prince@gmail.com",
+    //     password:"prince123"
+    // }
+
+    // // Creating the instance of User Model
+    // const user=new User(userObj)    (OR directly)
+
+    const user = new User({
+        firstName: "Prince",
+        lastName: "Kushwaha",
+        emailId: "prince@gmail.com",
+        password: "prince123"
+    })
+    try {
+        await user.save();
+        res.send("User Added Successfully!!!")
+    } catch (err) {
+        res.status(400).send("Error saving the User" + err.message)
+    }
+
+    
+})
+
+// First database should be connected then server should start and start listening API
+connectDB().then(() => {
+    console.log("Database Connected...")
+    app.listen(5555, () => {
+        console.log("Server is running on port 5555")
+    })
+}).catch((err) => {
+    console.log("Database connection not established!!")
+})
+
+// app.listen(5555, () => {
+//     console.log("Server is running on port 5555")
+// })
---
+++ src/db/database.js
@@ -0,0 +1,7 @@
+const mongoose=require('mongoose');
+
+const connectDB=async()=>{
+    await mongoose.connect("mongodb+srv://princeking19ald:prince123@cluster0.taxs0.mongodb.net");
+}
+
+module.exports=connectDB;
