4e4f5d2f14da64f736da55681a501c86ddd71e83
imprince-kushwaha <mijadon593@hidelux.com>
2024-10-14 13:12:33 +0530

    Get Dynamic data from body

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const app = express();
 
 const User = require("./models/user.model")
 
+app.use(express.json());
+
 app.post('/signup', async (req, res) => {
     // const userObj={
     //     firstName:"Prince",
@@ -16,12 +18,23 @@ app.post('/signup', async (req, res) => {
     // // Creating the instance of User Model
     // const user=new User(userObj)    (OR directly)
 
-    const user = new User({
-        firstName: "Prince",
-        lastName: "Kushwaha",
-        emailId: "prince@gmail.com",
-        password: "prince123"
-    })
+    // const user = new User({
+    //     firstName: "Prince",
+    //     lastName: "Kushwaha",
+    //     emailId: "prince@gmail.com",
+    //     password: "prince123"
+    // })
+    // try {
+    //     await user.save();
+    //     res.send("User Added Successfully!!!")
+    // } catch (err) {
+    //     res.status(400).send("Error saving the User" + err.message)
+    // }
+
+
+    // PASS DYNAMIC DATA TO API
+    console.log(req.body)  //will give 'undefined' initially,becoz it's in json format to read json data so we use express.json to get data
+    const user = new User(req.body);
     try {
         await user.save();
         res.send("User Added Successfully!!!")
